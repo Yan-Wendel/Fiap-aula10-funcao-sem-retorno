9294d1949551af219129abc08386aee10cb94fca
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-11 14:01:52 -0300

    fu

--- Fiap_aula10_fucao/src/Exercicios_funcao/exercicio1.java
+++ Fiap_aula10_fucao/src/Exercicios_funcao/exercicio1.java
@@ -1,10 +1,16 @@
 package Exercicios_funcao;
 
 public class exercicio1 {
-
+/*▪ Faça uma função com o nome msgErro que, quando chamada, exibe a seguinte
+mensagem na tela;
+▪ “Essa é uma mensagem padronizada de erro. Cuidado com o que está fazendo!!”*/
+	//criar fucao.
+	static void msgErro() {
+		System.out.println("Essa é uma mensagem padronizada de erro. Cuidado com o que está fazendo!!");
+	} 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//chamar a fução.
+		msgErro();
 	}
 
 }
