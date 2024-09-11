a4c1612d08f4f1d43da39da59ebe12c185723768
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-11 13:43:47 -0300

    ex

---
+++ Fiap_aula10_fucao/src/Exercicios_funcao/exercicio1.java
@@ -0,0 +1,10 @@
+package Exercicios_funcao;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Fiap_aula10_fucao/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Fiap_aula10_fucao {
+}
