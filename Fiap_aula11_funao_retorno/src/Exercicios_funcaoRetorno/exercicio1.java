204e1d0be0246ebc083009b8ab0d3f01b440b02b
Yan-Wendel <yanwendelrodriguesmachado@gmail.com>
2024-09-11 14:55:51 -0300

    ex

---
+++ Fiap_aula11_funao_retorno/src/Exercicios_funcaoRetorno/exercicio1.java
@@ -0,0 +1,10 @@
+package Exercicios_funcaoRetorno;
+
+public class exercicio1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Fiap_aula11_funao_retorno/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Fiap_aula11_funao_retorno {
+}
